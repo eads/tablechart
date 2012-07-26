ea49109796bb1071ad1f06be2fb829540ef31dcf
David Eads <davideads@gmail.com>
2012-07-26 01:38:51 -0500

    almost working code

--- jquery.tablechart.js
+++ jquery.tablechart.js
@@ -1,4 +1,6 @@
-(function($, Tablechart) {
+var Tablechart;
+
+(function($) {
 
 var uid = 0;
 function getUID() {
@@ -16,11 +18,9 @@ $.fn.tablechart = function(options) {
     var charts = $(this).data('tablechart') || {};
     if (charts[options.chartName] == undefined) {
       charts[options.chartName] = new Tablechart(this, options);
-      $(this).data('tablechart', charts);
     } 
-    else {
-      charts[options.chartName].plot();
-    }
+    charts[options.chartName].plot();
+    $(this).data('tablechart', charts);
   });
   return this;
 };
@@ -28,9 +28,8 @@ $.fn.tablechart = function(options) {
 Tablechart = function(el, options) {
   this.options = options;
   this.el = el;
-  this.$el = $(el);
   this.chart_id = 'chart-' + getUID();
-  this.offset = 0;
+  this.data = [];
   
   this.chart = this.create();
   this.plot();
@@ -39,195 +38,77 @@ Tablechart = function(el, options) {
 
 Tablechart.prototype.create = function() {
   // Create container
-  var chartContainer = $('<div class="tablechart">').attr('id', this.chart_id).insertBefore(this.$el);
-
+  var chartContainer = $('<div class="tablechart">').attr('id', this.chart_id).insertBefore($(this.el));
   if (this.options.height) { chartContainer.height(options.height); }
   if (this.options.width)  { chartContainer.width(options.width); }
-  
-  return $.jqplot(this.chart_id, [[[]]], this.options.plotOptions);
-   
 }
 
 Tablechart.prototype.plot = function() {
+  this.offset = 0;
+  tablechart = this;
   // Consolidate plotting here 
   if (!$.nodeName(this.el, 'table')) {
-    //table = $('table', this.el);
+    $('table', this.el).each(function() {
+      tablechart.scrape(this);
+    });
   } else {
-    //this.offset = 0;
-    //this.scrape(this.el);
+    tablechart.scrape(this.el);
   }
-  //console.log(this.chart);
-  //var chart = this.chart;
-  this.chart.series[0].data = [[2,3],[3,4],[4,5]];
-  console.log(this.chart.data);
-  //this.chart.init(this.chart_id, this.chart.data); // this.options.plotOptions);
-  
-  this.chart.reInitialize();
-  this.chart.replot(this.options.replotOptions);
-  this.chart.redraw(); //this.options.replotOptions);
-
+  if (this.chart) {
+    this.chart.destroy();
+  }
+  this.chart = $.jqplot(this.chart_id, this.data, this.options.plotOptions);
 }
 
 /**
  * Utility function: Scrape single table for values
  */
 Tablechart.prototype.scrape = function(table) {
-  var chart = this.chart,
+  var tablechart = this,
       options = this.options,
       offset = this.offset,
-      series = this.series;
+      data = this.data;
 
-  if (options.parseOptions) {
-    $(table).find('thead th:gt(0)').each(function(i) {
-      var idx = offset + i, dataOptions = {};
-        dataOptions = Tablechart.replaceOptions($(this).data('seriesoptions'));
-      options.plotOptions.series[idx] = $.extend({
-        label: $(this).text()}, 
-        options.plotOptions.series[idx],
-        dataOptions
-      );
-    });
-  }
+  $(table).find('thead th:gt(0)').each(function(i) {
+    var idx = offset + i, dataOptions = {};
+    if (options.parseOptions) {
+      dataOptions = Tablechart.replaceOptions($(this).data('seriesoptions'));
+    }
+    options.plotOptions.series[idx] = $.extend({
+      label: $(this).text()}, 
+      options.plotOptions.series[idx],
+      dataOptions
+    );
+  });
 
   $(table).find('tbody tr').each(function(i) {
     var x = 0, y = 0;
     $(this).find('th').each(function() {
-      x = options.parseX.call(chart, this);
+      x = options.parseX.call(tablechart, this);
     });
     $(this).find('td').each(function(j) {
-      idx = offset + j; // + j;
-      //if (chart.series[idx] == undefined) {
-      //  chart.series[idx] = $.extend({}, chart.series[0]);
-      //}
-      //if (i == 0) {
-      //  offset = offset + 1;
-      //  chart.series[idx].data = [];
-      //}
-      //if chart.series[0].data = [];
-      y = options.parseY.call(chart, this);
-      //if (y == NaN) 
-      //console.log(y);
-      //console.log(this);
-      //}
-      //console.log(x);
-      //console.log(y);
-      if (i == 0 && chart.data[idx] == undefined) {
-          chart.data[idx] = [];
-      } 
-      chart.data[idx].push([x, y]);
+      idx = offset + j
+      if (i == 0) {
+        data[idx] = [];
+      }
+      y = options.parseY.call(tablechart, this);
+      data[idx].push([x, y]);
     });
   });
 }
-
-
-
-  
-  //if (this.chart) {
-    //this.offset = 0;
-    //this.series = [];
-  //}
-    //// Is matched element a table?
-  //if (!$.nodeName(this.el, 'tabldraw {
-    //tables = $('table', this.el);
-    //this.scrapeMultiple.call(this, tables);
-  //} else {
-    //tables = $(this.el);
-    //this.scrapeSingle.call(this, tables);
-  //}
-
-//}
-
-
-
-/** 
- * Tablechart draw method
- */
-/*$.tablechart.prototype.draw = function() {
-  var tables, data;
-
-  if (!this.chart) {
-    this.chart = $.jqplot(this.chartId, this.series, this.options.plotOptions);
-  }
-  else {
-    for (i=0; i < this.series.length; i++) {
-      if (this.chart.series[i] != undefined) {
-        this.chart.series[i].data = this.series[i];
-        this.chart.replot(this.options.replotOptions);  
-      }
-    }
-  }
-}*/
-
-/**
- * Utility function: Scrape single table for values
- */
-//$.tablechart.scrapeSingle = function(table) {
-  //var series = [],
-      //options = this.options,
-      //tablechart = this,
-      //offset = this.offset;
-
-  //if (options.headerSeriesLabels) {
-    //$(table).find('thead th:gt(0)').each(function(i) {
-      //var idx = offset + i,
-          //dataOptions = $.tablechart.replaceOptions($(this).data('seriesoptions'));
-      //options.plotOptions.series[idx] = $.extend(
-        //{label: $(this).text()}, 
-        //options.plotOptions.series[idx],
-        //dataOptions
-      //);
-    //});
-  //}
-
-  //$(table).find('tbody tr').each(function(i) {
-    //var x = 0, y = 0;
-    //$(this).find('th').each(function() {
-      //x = options.parseX.call(tablechart, this);
-    //});
-    //$(this).find('td').each(function(j) {
-      //idx = offset + j;
-      //if (!tablechart.series[idx]) {
-        //tablechart.series[idx] = [];
-      //}
-      //y = options.parseY.call(tablechart, this);
-      //tablechart.series[idx].push([x, y]);
-      //if (i == 0) {
-        //tablechart.offset++;
-      //}
-    //});
-  //});
-//}
-
-/**
- * Utility function: Scrape multiple tables for values
- */
-//$.tablechart.scrapeMultiple = function(tables) {
-  //var series = [],
-      //options = this.options,
-      //tablechart = this,
-      //seriesOptions = [];
-
-  //$(tables)
-    //.not('.jqplot-target table') // Filter out jqplot-added tables
-    //.each(function(i) {
-      //var table = this;
-      //// Scrape each matched table
-      //$.tablechart.scrapeSingle.call(tablechart, this);
-    //});
-//}
 
 /**
  * Utility function: Parse text
  */
 Tablechart.parseText = function(el) {
-  return $(el).text();
+  return el.innerText;
 }
 
 /**
  * Utility function: Parse text to floating point
  */
 Tablechart.parseFloat = function(el) {
-  return parseFloat($(el).text());
+  return parseFloat(el.innerText);
 }
 
 Tablechart.REPLACE = ['renderer', 'markerRenderer', 'labelRenderer', 'parseX',
@@ -259,7 +140,6 @@ Tablechart.replaceOptions = function(obj) {
   });
   return obj;
 }
-
 
 /**
  * Defaults
